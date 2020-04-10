7d17ae4b529b7cf4c04dbc52b78c68eaaa7515fa
Adi K <kadimetla@gmail.com>
2020-04-09 20:25:54 -0400

    add create contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ContactListComponent} from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
 
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
   {path:"home",component:HomeComponent},
-  {path:"contact-list",component:ContactListComponent}
+  {path:"contact-list",component:ContactListComponent},
+  {path:"contact-create",component:ContactCreateComponent}
   
 ]
 
